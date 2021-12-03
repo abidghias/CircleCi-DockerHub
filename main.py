98d1a6b95e54fc8a4bcd07aeeec63374349473f2
Mohamed Ghias <v-mghias@microsoft.com>
2021-12-03 14:45:35 +0530

    updated main.py again1

--- main.py
+++ main.py
@@ -24,15 +24,14 @@ CORS(app)
 def index():
     return "Flask app is running with a few changes "
 
-#port = int(os.getenv("PORT", 5001))
-
 if __name__ == "__main__":
-    #host = '0.0.0.0'
+    host = '0.0.0.0'
+    port = int(os.getenv("PORT",62356))
     #app.run(debug=True)
 
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    #app.run(host='0.0.0.0', debug=True)
 
-    #httpd = simple_server.make_server(host=host, app=app)
+    httpd = simple_server.make_server(host=host,port = port, app=app)
     #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
     #print("Serving on %s %d" % (host))
-    #httpd.serve_forever()
+    httpd.serve_forever()
